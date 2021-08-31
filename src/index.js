caec4422ba063c4bd3eec2c130f31f8afb44becb
RegioNT <54860011+RegioNT@users.noreply.github.com>
2021-08-31 03:20:48 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -9,27 +9,37 @@ let bolas = [
     {id: 3, nome: 'All-court', tipo: 'Basquete', marca: 'Adidas'},
     {id: 4, nome: 'NCAA', tipo: 'Basquete', marca: 'Wilson'},
 ]
+function log(request, response, next){
+    const{url, method} = request;
+    console.log(`${method} - ${url} at ${new Date()}`)
+    return next();
+}
+app.use(log)
 
 /**
  * Retorna todas bolas em JSON 
  */
-
-app.get('/bolas', (request, response) => response.json(bolas))
+app.get('/bolas', (request, response) => response.status(200).json(bolas))
 
 /**
  * Buscar UM único recurso
  */
 app.get('/bolas/:id', (request, response) => {
-    const bola = bolas.filter(value => value.id == request.params.id);
-    response.json(bola);
+    const {id} = request.params;
+    const bola = bolas.find(value => value.id == id);
+    if(bola == undefined) {
+        response.status(400).send({error: 'Requisição invalida'});
+    }else{
+        response.status(200).json(bola);
+    }
 })
 /**
  * Inserir dados no servidor
  */
 app.post('/bolas', (request, response) => {
-  const bola = request.body;
-  bolas.push(bola);
-  response.json(bola);
+    const bola = request.body;
+    bolas.push(bola);
+    response.status(201).json(bola);
 })
 
 /**
@@ -39,17 +49,25 @@ app.post('/bolas', (request, response) => {
     const id = request.params.id;
     const nome = request.body.nome;
 
-    let bola = bolas.filter(value => value.id == id);
-
-    bola[0].nome = nome;
-
-    response.json(bola[0]);
+    let bola = bolas.find(value => value.id == id);
+    if(bola == undefined){
+        response.status(400).send();
+    }else{
+        bola.nome = nome;
+        response.status(200).json(bola);
+    }
 })
 
 app.delete('/bolas/:id', (request, response) => {
-    const id = request.params.id;
-    bolas = bolas.filter(value => value.id != id);
-    response.json(bolas);
+    const {id} = request.params;
+    const index = bolas.findIndex(value => value.id == id);
+    if(index == -1){
+        response.status(400).send();
+    }else{
+        bolas.splice(index, 1);
+        response.status(204).send();
+    }
+
 })
 
 app.listen(3000);
